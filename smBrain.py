483a05ed5d24b00787b71f514346239615b775a8
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2020-12-30 23:00:08 +0300

    Sonars

---
+++ smBrain.py
@@ -0,0 +1,70 @@
+import math
+import lib601.util as util
+import lib601.sm as sm
+import lib601.gfx as gfx
+from soar.io import io
+
+class MySMClass(sm.SM):
+    coeff = 5
+    fvelMax = 0.3
+    tolerance = 0.001
+
+    def __init__(self, targetDist):
+        self.targetDist = targetDist
+        self.startState = 5
+
+    def getNextValues(self, state, inp):
+        currentDist = inp.sonars[3]
+        delta = currentDist - self.targetDist
+
+        if not self.done(state):
+            fvel = self.coeff * delta * self.fvelMax
+            fvel = max(min(fvel, self.fvelMax), -self.fvelMax)
+            return (currentDist, io.Action(fvel = fvel, rvel = 0))
+        else:
+            return (currentDist, None)
+
+    
+    def done(self, state):
+        return abs(state - self.targetDist) <= self.tolerance
+
+mySM = MySMClass(0.5)
+mySM.name = 'brainSM'
+
+######################################################################
+###
+###          Brain methods
+###
+######################################################################
+
+def plotSonar(sonarNum):
+    robot.gfx.addDynamicPlotFunction(y=('sonar'+str(sonarNum),
+                                        lambda: 
+                                        io.SensorInput().sonars[sonarNum]))
+
+# this function is called when the brain is (re)loaded
+def setup():
+    robot.gfx = gfx.RobotGraphics(drawSlimeTrail=True, # slime trails
+                                  sonarMonitor=False) # sonar monitor widget
+    
+    # set robot's behavior
+    robot.behavior = mySM
+
+# this function is called when the start button is pushed
+def brainStart():
+    robot.behavior.start(traceTasks = robot.gfx.tasks())
+
+# this function is called 10 times per second
+def step():
+    inp = io.SensorInput()
+    print inp.sonars[3]
+    robot.behavior.step(inp).execute()
+    io.done(robot.behavior.isDone())
+
+# called when the stop button is pushed
+def brainStop():
+    pass
+
+# called when brain or world is reloaded (before setup)
+def shutdown():
+    pass
